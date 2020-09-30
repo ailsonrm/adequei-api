4e4b47aed734252e04cf22963e208ed20eea9571
Ailson Ramos Moreira <ailson.rmoreira@gmail.com>
2020-09-30 19:53:58 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ dotenv.config()
 const appHost = process.env.HOST
 const port = process.env.PORT || 3333
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   console.log(`⚡️ Server listening on ${appHost}:${port}`)
 })
