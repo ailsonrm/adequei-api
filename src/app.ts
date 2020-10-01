bfa573b33389d1c4f533f9ad8ed9d54fdc897ccb
Ailson Ramos Moreira <ailson.rmoreira@gmail.com>
2020-10-01 01:28:01 -0300

    remove express-actuator

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express'
 import routes from '@routes/router'
 import cors from 'cors'
-import actuator from 'express-actuator'
 
 const app = express()
 
 app.use(cors())
-app.use(actuator())
 app.use(express.json())
 app.use(routes)
 app.use(express.static('views/images'))
